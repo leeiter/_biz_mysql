bd3b05b3309a6f4b313be4f7783e3abdb96bdd5f
leeiter <iterlees@gmail.com>
2020-04-16 16:58:43 +0900

    mypage update

--- mysql(2020-04-08-secur).sql
+++ mysql(2020-04-08-secur).sql
@@ -23,7 +23,7 @@ CREATE TABLE users(
 -- 2020-04-10 변경
 -- tbl_users 테이블 변경
 -- user_name 칼럼 UNIQUE로 변경
--- DROP TABLE tbl_users;
+DROP TABLE tbl_users;
 CREATE TABLE tbl_users(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	user_name VARCHAR(50) UNIQUE,
@@ -32,13 +32,38 @@ CREATE TABLE tbl_users(
 );
 
 -- 로그인한 User의 권한들을 보관할 TABLE
--- DROP TABLE authorities;
+DROP TABLE authorities;
 CREATE TABLE authorities(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	username VARCHAR(50),
 	authority VARCHAR(50)
 );
 
-DELETE FROM tbl_users  WHERE id = 3;
+DELETE FROM tbl_users  WHERE id = 5;
+DELETE FROM authorities  WHERE username = 'admin';
 SELECT * FROM tbl_users;
+SELECT * FROM authorities;
+
+
 COMMIT;
+
+
+DELETE FROM tbl_users WHERE user_name = 'admin';
+INSERT INTO tbl_users ( user_name, user_pass, enabled, email, phone, address )
+VALUES ( 'admin', 'admin', true, 'a@a.com', '010-1111-1111', '광주광역시' );
+
+INSERT INTO authorities ( username, authority )
+VALUES ( 'admin', 'ADMIN' );
+
+INSERT INTO authorities ( username, authority )
+VALUES ( 'admin', 'ROLE_ADMIN' );
+
+INSERT INTO tbl_users ( user_name, user_pass, enabled, email, phone, address )
+VALUES ( 'user', 'user', true, 'user@user.com', '010-2222-2222', '광주광역시' );
+
+INSERT INTO authorities ( username, authority )
+VALUES ( 'user', 'USER' );
+
+INSERT INTO authorities ( username, authority )
+VALUES ( 'user', 'ROLE_USER' );
+
